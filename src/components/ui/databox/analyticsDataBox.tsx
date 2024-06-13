68ce6c74798e2c915d1f8c3ba937acf9811fc581
Codec bro 8 <codec.bro8@gmail.com>
2024-06-13 15:59:44 +0100

    build errorfix

--- src/components/ui/databox/analyticsDataBox.tsx
+++ src/components/ui/databox/analyticsDataBox.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Flex, Text, Icon } from '@chakra-ui/react';
 import { FaChurch, FaUsers, FaDollarSign, FaBell } from 'react-icons/fa'; // Example icons, adjust as necessary
-import styles from '@/styles/DataBox.module.scss';
+
 
 interface DataBoxProps {
   type: 'video' | 'music' | 'blog' | 'devotional';
@@ -38,18 +38,17 @@ const AnalyticsDataBox: React.FC<DataBoxProps> = ({ type, data }) => {
       <Flex
         align="center"
         justifyContent="center"
-        className={styles.iconBox}
         bg={bgColor}
         borderRadius="50%"
         p='1.2rem'
         w='3.4rem' h='3.4rem'
         color='black.1'
       >
-        <Icon as={IconComponent} className={styles.icon} fontSize='28px' />
+        <Icon as={IconComponent}  fontSize='28px' />
       </Flex>
-      <Flex className={styles.textBox} justify='start' flexDirection="column" alignItems="start">
+      <Flex  justify='start' flexDirection="column" alignItems="start">
         <Text color='white' fontSize="2xl" fontWeight="bold" >{data}</Text>
-        <Text className={styles.label}>Total {type.charAt(0).toUpperCase() + type.slice(1)}</Text>
+        <Text >Total {type.charAt(0).toUpperCase() + type.slice(1)}</Text>
       </Flex>
     </Box>
   );
